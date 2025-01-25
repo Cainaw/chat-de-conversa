fd729f06717b2409537c31b0e1ec8db377dd4eeb
Cainã <cainamwc@gmail.com>
2025-01-25 01:33:14 -0300

    subpacote responsavel pela transferencia de dados do servidor

--- Comunicação/src/servidor/Servidor.java
+++ Comunicação/src/servidor/Servidor.java
@@ -1,57 +1,36 @@
 package servidor;
-
+ 
+import servidor.comunicação.*;
 import java.io.*;
 import java.net.*;
 
 public class Servidor {
 	public static void main(String[] args) {
-		int porta = 10000;
+		final int porta = 10000;
+		
+		
+		GestorDeClientes gestor = new GestorDeClientes(5);
 		
 		try (ServerSocket servidor = new ServerSocket(porta)) {
+			
 			Socket cliente;
-			System.out.println("Esperando conexão...\n");
+			RecebimentoDeMensagem rec = new RecebimentoDeMensagem(servidor);
 			
-			cliente = servidor.accept();
-			System.out.println("Cliente conectado\n");
-			
-			try (
-				BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
-				BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
-				PrintWriter saida = new PrintWriter(cliente.getOutputStream(), true);
-			) {
-				System.out.println("- Chat de comunicação ativo -");
-				String mensagem;
-				while ((mensagem = entrada.readLine()) != null) {
-					System.out.println(mensagem);
-					
-					if (mensagem.equalsIgnoreCase("sair")) {
-						System.out.println("Cliente desconectado...");
-						break;
-					}
-					
-					System.out.print("Servidor: ");
-					mensagem = teclado.readLine();
-					
-					saida.println("Servidor: " + mensagem);
+			while (true) {
+				cliente = servidor.accept();
+				
+				if (cliente.isConnected()) {
+					gestor.adicionar(cliente);
 				}
 				
+				
+				
 			}
-			catch (IOException e) {
-				System.err.println("Erro ao se comunicar com o cliente: " + e.getMessage());
-			}
-			finally {
-				try {
-					cliente.close();
-					System.out.println("Conexão encerrada");
-				}
-				catch (IOException e) {
-					System.err.println("Erro ao fechar o cliente: " + e.getMessage());
-				}
-			}
+			
 		}
 		
 		catch (IOException e) {
-			System.err.println("Erro ao iniciar o cliente: " + e.getMessage());
+			System.err.println("Erro ao iniciar o servidor: " + e.getMessage());
 		}
 		
 	}
