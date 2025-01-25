d555a4705e887d3991574f26a9fbb2ca8ccfcb5f
Cainã <cainamwc@gmail.com>
2025-01-25 16:44:58 -0300

    gerencia corretamente os dados do clientes

--- Comunicação/src/servidor/GestorDeClientes.java
+++ Comunicação/src/servidor/GestorDeClientes.java
@@ -3,60 +3,63 @@ package servidor;
 import java.io.*;
 import java.net.*;
 
-public class GestorDeClientes{
-	final int tamanho;
-	Thread [] clientes;
+public class GestorDeClientes implements Runnable {
+	Socket cliente;
+	PrintWriter saida;
+	PrintWriter [] lista;
 	
-	public GestorDeClientes(int tamanho) {
-		this.tamanho = tamanho;
-		this.clientes = new Thread[tamanho];
+	public GestorDeClientes (Socket cliente) {
+		this.cliente = cliente;
 	}
 	
-	public boolean cheio() {
-		return this.clientes.length == this.tamanho;
-	}
-	
-	public boolean adicionar(Socket novoCliente) {
-		if (cheio()) {
-			try {
-				novoCliente.close();
-			} 
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-			return false;
-		}
-		
-		ThreadCliente cliente = new ThreadCliente(novoCliente);
-		for (int indice = 0; indice < this.tamanho; indice++) {
-			if (clientes[indice].isAlive()) {
-				this.clientes[indice] = new Thread(cliente);
-				cliente.setIdentidade(indice);
+	@Override
+	public void run() {
+		try (
+			BufferedReader entrada = new BufferedReader(new InputStreamReader(this.cliente.getInputStream()));
+			PrintWriter saida = new PrintWriter(this.cliente.getOutputStream(), true);
+		) {
+			this.saida = saida;
+			this.adicionarSaida();
+			
+			String mensagem;
+			int indice;
+			while ((mensagem = entrada.readLine()) != null) {
+				System.out.println(mensagem);
 				
-				this.clientes[indice].start();
-				return true;
+				indice = 0;
+				for (PrintWriter celula : lista) {
+					if (celula != null && saida != lista[indice]) {
+						celula.println(mensagem);
+					} indice++;
+				}
 			}
 		}
-		return false;
-	}
-	
-	
-	// arrumar método
-	public boolean remover(int indice) {
-		return false;
-	}
-	
-	// arrumar método
-	public ThreadCliente getCliente (int id) {
-		int indice = 0;
-		for (Thread cliente : clientes) {
-			if (indice++ == id)
-				return null;
+		catch (IOException e) {
+			System.err.println("Erro ao criar canal de comunicação: " + e.getMessage());
 		}
-		return null;
+		finally {
+			try {
+				cliente.close();
+				System.out.println("Cliente fechado com sucesso");
+			}
+			catch (IOException e) {
+				System.err.println("Erro ao fechar o cliente: " + e.getMessage());
+			}
+		}
 	}
 	
-	public int getTamanho() {
-		return this.tamanho;
+	public void adicionarLista(PrintWriter [] lista) {
+		this.lista = lista;
+	}
+	
+	private boolean adicionarSaida() {
+		int indice = 0;
+		for (PrintWriter celula : lista) {
+			if (celula == null) {
+				lista[indice] = saida;
+				return true;
+			} indice++;
+		}
+		return false;
 	}
 }
