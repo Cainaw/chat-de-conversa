dec3d7cbca2e96188b2c501fd95594029d758bc4
Cainã <cainamwc@gmail.com>
2025-01-25 22:45:12 -0300

    Adicionada uma GUI ainda incompleta

--- Comunicação/src/interfaces/Cliente.java
+++ Comunicação/src/interfaces/Cliente.java
@@ -5,7 +5,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JTextPane;
+//import javax.swing.JTextPane;
 import java.awt.SystemColor;
 import javax.swing.JTextField;
 import javax.swing.JTextArea;
